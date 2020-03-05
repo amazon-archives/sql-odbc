0ee22c489f796bb80bc28ba14355039c4c0aa0fb
lyndonb-bq <lyndonb@bitquilltech.com>
2020-03-05 11:47:36 -0800

    [1] Fixed path [2] Fixed output

--- src/UnitTests/UTConn/test_conn.cpp
+++ src/UnitTests/UTConn/test_conn.cpp
@@ -128,18 +128,22 @@ TEST(TestESConnDropDBConnection, ValidParameters) {
 }
 
 int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
+    //::testing::InitGoogleTest(&argc, argv);
+    std::cout << "CaptureStdout\n" << std::endl;
     testing::internal::CaptureStdout();
 
+    std::cout << "Starting tests\n" << std::endl;
     // Run test and pipe output to a file
     ::testing::InitGoogleTest(&argc, argv);
+    std::cout << "RUN_ALL_TESTS\n" << std::endl;
     int failures = RUN_ALL_TESTS();
+    std::cout << "done\n" << std::endl;
 
-    std::string output = testing::internal::GetCapturedStdout();
-    std::cout << output << std::endl;
+    //std::string output = testing::internal::GetCapturedStdout();
+    //std::cout << output << std::endl;
     std::cout << (failures ? "Not all tests passed." : "All tests passed")
               << std::endl;
-    WriteFileIfSpecified(argv, argv + argc, "-fout", output);
+    //WriteFileIfSpecified(argv, argv + argc, "-fout", output);
 
     return failures;
 }
