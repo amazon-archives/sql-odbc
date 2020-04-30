802a2f16d4c9a8b722e44383833c95af2b64475b
Rupal Mahajan <rupalm@bitquilltech.com>
2020-04-30 14:53:58 -0700

    fix mac build

--- src/UnitTests/UTConn/test_query_execution.cpp
+++ src/UnitTests/UTConn/test_query_execution.cpp
@@ -39,7 +39,7 @@ const std::string fetch_size = "0";
 const int all_columns_flights_count = 25;
 const int some_columns_flights_count = 2;
 runtime_options valid_conn_opt_val = {
-    {valid_host, valid_port, "1"},
+    {valid_host, valid_port, "1", "0"},
     {"BASIC", valid_user, valid_pw, valid_region},
     {use_ssl, false, "", "", "", ""}};
 
