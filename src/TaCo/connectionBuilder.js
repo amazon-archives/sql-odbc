638af61be277ea8691b9382f872935676587cd27
Rupal Mahajan <rupalm@bitquilltech.com>
2020-04-30 17:41:09 -0700

    login with basic auth

---
+++ src/TaCo/connectionBuilder.js
@@ -0,0 +1,40 @@
+(function dsbuilder(attr)
+{
+    var params = {};
+
+    params["SERVER"] = attr[connectionHelper.attributeServer];
+    params["PORT"] = attr[connectionHelper.attributePort];
+    params["UID"] = attr[connectionHelper.attributeUsername];
+    params["PWD"] = attr[connectionHelper.attributePassword];
+    params["BOOLSASCHAR"] = "0";
+    params["LFCONVERSION"] = "0";
+    params["UseDeclareFetch"] = "1";
+    params["Fetch"] = "2048";
+
+    if (attr[connectionHelper.attributeSSLMode] == "require")
+    {
+        params["sslmode"] = "require";
+    }
+
+    var odbcConnectStringExtrasMap = {};
+    const attributeODBCConnectStringExtras = "odbc-connect-string-extras";
+    if (attributeODBCConnectStringExtras in attr)
+    {
+        odbcConnectStringExtrasMap = connectionHelper.ParseODBCConnectString(attr[attributeODBCConnectStringExtras]);
+    }
+    for (var key in odbcConnectStringExtrasMap)
+    {
+        params[key] = odbcConnectStringExtrasMap[key];
+    }
+
+    var formattedParams = [];
+
+    formattedParams.push(connectionHelper.formatKeyValuePair(driverLocator.keywordDriver, driverLocator.locateDriver(attr)));
+
+    for (var key in params)
+    {
+        formattedParams.push(connectionHelper.formatKeyValuePair(key, params[key]));
+    }
+
+    return formattedParams;
+})
