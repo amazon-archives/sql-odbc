f9b24aa16d5f0b68d1176ce4c842ce5d39077c93
Rupal Mahajan <rupalm@bitquilltech.com>
2020-05-05 12:45:35 -0700

    - aws auth addition - additional options

--- src/TaCo/connectionBuilder.js
+++ src/TaCo/connectionBuilder.js
@@ -1,45 +1,43 @@
-(function dsbuilder(attr)
-{
+(function dsbuilder(attr){
     var params = {};
 
+    // Set host information in connection string
     params["SERVER"] = attr[connectionHelper.attributeServer];
     params["PORT"] = attr[connectionHelper.attributePort];
-    params["UID"] = attr[connectionHelper.attributeUsername];
-    params["PWD"] = attr[connectionHelper.attributePassword];
 
+    // Set authentication values in connection string
     var authAttrValue = attr[connectionHelper.attributeAuthentication];
     if (authAttrValue == "auth-none"){
-        // no-op
-    } else if (authAttrValue == "basic")
-    {
+        params["Auth"] = "NONE";
+    } else if (authAttrValue == "AWS_SIGV4"){
+        params["Auth"] = "AWS_SIGV4";
+        params["Region"] = attr[connectionHelper.attributeVendor2];
+    } else if (authAttrValue == "BASIC"){
+        params["Auth"] = "BASIC";
         params["UID"] = attr[connectionHelper.attributeUsername];
         params["PWD"] = attr[connectionHelper.attributePassword];
     }
 
-    if (attr[connectionHelper.attributeSSLMode] == "require")
-    {
-        params["sslmode"] = "require";
+    // Set SSL value in connection string 
+    if (attr[connectionHelper.attributeSSLMode] == "require"){
+        params["useSSL"] = "1";
     }
 
+    // Parse additional options and add in connection string
     var odbcConnectStringExtrasMap = {};
-    const attributeODBCConnectStringExtras = "odbc-connect-string-extras";
-    if (attributeODBCConnectStringExtras in attr)
-    {
+    const attributeODBCConnectStringExtras = "vendor1";
+    if (attributeODBCConnectStringExtras in attr){
         odbcConnectStringExtrasMap = connectionHelper.ParseODBCConnectString(attr[attributeODBCConnectStringExtras]);
     }
-    for (var key in odbcConnectStringExtrasMap)
-    {
+    for (var key in odbcConnectStringExtrasMap){
         params[key] = odbcConnectStringExtrasMap[key];
     }
 
+    // Format the attributes as 'key=value'
     var formattedParams = [];
-
     formattedParams.push(connectionHelper.formatKeyValuePair(driverLocator.keywordDriver, driverLocator.locateDriver(attr)));
-
-    for (var key in params)
-    {
+    for (var key in params){
         formattedParams.push(connectionHelper.formatKeyValuePair(key, params[key]));
     }
-
     return formattedParams;
 })
